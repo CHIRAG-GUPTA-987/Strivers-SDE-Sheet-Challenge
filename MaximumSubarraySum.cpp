4525854e81d9ad16b22e953c574f623e417d2141
Chirag Gupta <91630704+CHIRAG-GUPTA-987@users.noreply.github.com>
2023-06-06 21:38:24 +0530

    Problem Solved: Maximum Subarray Sum

---
+++ MaximumSubarraySum.cpp
@@ -0,0 +1,19 @@
+long long maxSubarraySum(int arr[], int n)
+{
+    /*
+        Don't write main().
+        Don't read input, it is passed as function argument.
+        No need to print anything.
+        Taking input and printing output is handled automatically.
+    */
+    long long sum = 0, s = 0;
+    for (int i = 0; i < n; i++)
+    {
+        s += arr[i];
+        if (s > sum)
+            sum = s;
+        if (s < 0)
+            s = 0;
+    }
+    return sum;
+}
