b07ed54f55de659ff4b62808cd340e91633ee20c
Chirag Gupta <91630704+CHIRAG-GUPTA-987@users.noreply.github.com>
2023-06-06 23:18:48 +0530

    Problem Solved: Sort 0 1 2

---
+++ Sort012.cpp
@@ -0,0 +1,26 @@
+void swap(int &a, int &b)
+{
+    int temp = a;
+    a = b;
+    b = temp;
+}
+void sort012(int *arr, int n)
+{
+    int low = 0, mid = 0, high = n - 1;
+    while (mid <= high)
+    {
+        if (arr[mid] == 0)
+        {
+            swap(arr[mid], arr[low]);
+            mid++, low++;
+        }
+        else if (arr[mid] == 2)
+        {
+            swap(arr[mid], arr[high]);
+            high--;
+        }
+        else
+            mid++;
+    }
+    //   Write your code here
+}
