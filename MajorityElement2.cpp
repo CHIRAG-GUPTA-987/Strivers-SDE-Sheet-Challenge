a8517ea06014c929e0ecbe412c410bba533fa31e
Chirag Gupta <91630704+CHIRAG-GUPTA-987@users.noreply.github.com>
2023-06-08 17:57:25 +0530

    Problem Update: Majority Element - 2 (using Moore's Voting Algorithm)

--- MajorityElement2.cpp
+++ MajorityElement2.cpp
@@ -1,19 +1,51 @@
 vector<int> majorityElementII(vector<int> &arr)
 {
+
+    // TIME = O(nlogn) SPACE = O(1)
+    // vector<int> ans;
+    // sort(arr.begin(), arr.end());
+    // int n=arr.size();
+    // for(int i=0; i<n; i++) {
+    //     int el = arr.at(i), count=1;
+    //     while(i<n-1 && arr.at(i+1) == el) {
+    //         i++;
+    //         count++;
+    //     }
+    //     if(count > n/3) ans.push_back(el);
+    // }
+    // return ans;
+
+    // TIME = O(n) SPACE = O(1)
     vector<int> ans;
-    sort(arr.begin(), arr.end());
-    int n = arr.size();
+    int el1 = INT_MIN, el2 = INT_MIN, cnt1 = 0, cnt2 = 0, n = arr.size();
     for (int i = 0; i < n; i++)
     {
-        int el = arr.at(i), count = 1;
-        while (i < n - 1 && arr.at(i + 1) == el)
+        if (cnt1 == 0 && el2 != arr.at(i))
+            el1 = arr.at(i);
+        else if (cnt2 == 0 && el1 != arr.at(i))
+            el2 = arr.at(i);
+        if (el1 == arr.at(i))
+            cnt1 += 1;
+        else if (el2 == arr.at(i))
+            cnt2 += 1;
+        else
         {
-            i++;
-            count++;
+            cnt1--;
+            cnt2--;
         }
-        if (count > n / 3)
-            ans.push_back(el);
     }
+    cnt1 = 0, cnt2 = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (el1 == arr.at(i))
+            cnt1++;
+        if (el2 == arr.at(i))
+            cnt2++;
+    }
+    if (cnt1 > n / 3)
+        ans.push_back(el1);
+    if (cnt2 > n / 3)
+        ans.push_back(el2);
     return ans;
     // Write your code here.
 }
