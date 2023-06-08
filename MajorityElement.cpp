84b20eeba3eac07428272b4a676964a37cd8569e
Chirag Gupta <91630704+CHIRAG-GUPTA-987@users.noreply.github.com>
2023-06-08 17:34:31 +0530

    Problem Update: Majority Element (using Moore's Voting Algorithm)

--- MajorityElement.cpp
+++ MajorityElement.cpp
@@ -1,17 +1,33 @@
 int findMajorityElement(int arr[], int n)
 {
-    sort(arr, arr + n);
+
+    // TIME = O(nlogn) SPACE = O(1)
+    // sort(arr, arr+n);
+    // for(int i=0; i<n; i++) {
+    // 	int el = arr[i], count = 1;
+    // 	while(i<n-1 && el == arr[i+1]){
+    // 		i++;
+    // 		count++;
+    // 	}
+    // 	if(count > n/2) return el;
+    // }
+    // return -1;
+
+    // TIME = O(n) SPACE = O(1) USING MOORE'S VOTING ALGORITHM
+    int element = 0, count = 0;
     for (int i = 0; i < n; i++)
     {
-        int el = arr[i], count = 1;
-        while (i < n - 1 && el == arr[i + 1])
-        {
-            i++;
+        if (count == 0)
+            element = arr[i];
+        if (element == arr[i])
             count++;
-        }
-        if (count > n / 2)
-            return el;
+        else
+            count--;
     }
-    return -1;
+    count = 0;
+    for (int i = 0; i < n; i++)
+        if (element == arr[i])
+            count++;
+    return count > n / 2 ? element : -1;
     // Write your code here.
 }
