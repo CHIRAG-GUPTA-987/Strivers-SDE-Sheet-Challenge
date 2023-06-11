2f7c3960d3ce8a12d00b9e6232111a1f8f59f78f
Chirag Gupta <91630704+CHIRAG-GUPTA-987@users.noreply.github.com>
2023-06-11 13:00:30 +0530

    Problem Updated: 3 Sum

--- 3Sum.cpp
+++ 3Sum.cpp
@@ -4,7 +4,7 @@ vector<vector<int>> findTriplets(vector<int> arr, int n, int K)
     sort(arr.begin(), arr.end());
     for (int i = 0; i < n - 2; i++)
     {
-        if (i != 0 && arr[i] == arr.at(i - 1))
+        if (i != 0 && arr[i] == arr[i - 1])
             continue;
         int j = i + 1, k = n - 1;
         while (j < k)
@@ -14,9 +14,9 @@ vector<vector<int>> findTriplets(vector<int> arr, int n, int K)
             {
                 ans.push_back(vector<int>({arr[i], arr[j], arr[k]}));
                 j++, k--;
-                while (j < k && arr[j] == arr.at(j - 1))
+                while (j < k && arr[j] == arr[j - 1])
                     j++;
-                while (j < k && arr[k] == arr.at(k + 1))
+                while (j < k && arr[k] == arr[k + 1])
                     k--;
             }
             else if (sum > K)
